577d4f0c7c01611803a89af861f416aea6106cd0
Jason Stapels <jstapels@us.ibm.com>
2022-12-12 09:17:45 -0500

    Day 12 Solution

--- src/Day12.kt
+++ src/Day12.kt
@@ -12,30 +12,40 @@ fun main() {
                 else -> it
             } }
 
-    var best: List<Pos>? = null
+    val best = mutableMapOf<Pos, List<Pos>>()
+
+    fun printMap(map: List<List<Char>>) {
+        map.forEach {
+            it.forEach { c -> print(c) }
+            println()
+        }
+    }
 
     fun move(map: List<List<Char>>, moves: List<Pos>) {
-        if (best != null && best!!.size < moves.size) return
+        val pos = moves.last()
+        val bestForPos = best[pos]
+        if (bestForPos != null && bestForPos.size <= moves.size) {
+            return
+        } else {
+            best[pos] = moves
+        }
 
         val maxCol = map[0].size - 1
         val maxRow = map.size - 1
-        val pos = moves.last()
-        val height = map.height(pos)
 
         if (map.get(pos) == 'E') {
-            if (best == null || moves.size < best!!.size) {
-                best = moves
+            if (bestForPos == null || moves.size < bestForPos.size) {
+                best[pos] = moves
                 return
             }
         }
 
-        val valid = height + 1
+        val valid = map.height(pos) + 1
 
         val up = Pos(pos.row - 1, pos.col)
         val down = Pos(pos.row + 1, pos.col)
         val left = Pos(pos.row, pos.col - 1)
         val right = Pos(pos.row, pos.col + 1)
-
 
         if (pos.row > 0 && map.height(up) <= valid && up !in moves) {
             move(map, moves + up)
@@ -51,34 +61,48 @@ fun main() {
         }
     }
 
-    fun start(map: List<List<Char>>): Pos {
-        map.indices
-            .forEach { y ->
-                map[y].indices
-                    .forEach { x ->
-                        if (map[y][x] == 'S') return Pos(y, x)
-                    }
-            }
-        throw IllegalStateException()
-    }
+    fun findAll(map: List<List<Char>>, ch: Char) =
+        map.flatMapIndexed { row, line ->
+            line.withIndex()
+                .filter { it.value == ch }
+                .map { (col, _) -> Pos(row, col) }
+        }
 
     fun parseInput(input: List<String>) =
         input.map { it.toList() }
 
+    fun invertMap(map: List<List<Char>>) =
+        map.map { line ->
+            line.map { when (it) {
+                'E' -> 'S'
+                'S', 'a' -> 'E'
+                else -> 'a' + ('z' - it)
+            }}
+        }
+
     fun part1(input: List<String>): Int {
+        best.clear()
         val map = parseInput(input)
-        val start = start(map)
+        val start = findAll(map, 'S').first()
+        val finish = findAll(map, 'E').first()
         move(map, listOf(start))
-        println("Best - $best")
-        return best!!.size - 1
+        return best[finish]!!.size - 1
     }
 
-    fun part2(input: List<String>) = 1
+    fun part2(input: List<String>): Int {
+        best.clear()
+        val map = invertMap(parseInput(input))
+        val start = findAll(map, 'S').first()
+        val finish = findAll(map, 'E')
+        move(map, listOf(start))
+        return finish.filter { it in best }
+            .minOf { best[it]!!.size - 1 }
+    }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day12_test")
     checkThat(part1(testInput), 31)
-    checkThat(part2(testInput), 1)
+    checkThat(part2(testInput), 29)
 
     val input = readInput("Day12")
     println(part1(input))
